255ba396b99cf86d4d245126283a74528089ae8a
shanshe <shanshe@gmail.com>
2025-12-28 18:20:50 +0100

    Upload bd files

---
+++ z3660-firmware/Z-TURN/vivado/z3660.srcs/sources_1/bd/design_1/ip/design_1_zstorm_0_0/sim/design_1_zstorm_0_0.v
@@ -0,0 +1,369 @@
+// (c) Copyright 1995-2021 Xilinx, Inc. All rights reserved.
+// 
+// This file contains confidential and proprietary information
+// of Xilinx, Inc. and is protected under U.S. and
+// international copyright and other intellectual property
+// laws.
+// 
+// DISCLAIMER
+// This disclaimer is not a license and does not grant any
+// rights to the materials distributed herewith. Except as
+// otherwise provided in a valid license issued to you by
+// Xilinx, and to the maximum extent permitted by applicable
+// law: (1) THESE MATERIALS ARE MADE AVAILABLE "AS IS" AND
+// WITH ALL FAULTS, AND XILINX HEREBY DISCLAIMS ALL WARRANTIES
+// AND CONDITIONS, EXPRESS, IMPLIED, OR STATUTORY, INCLUDING
+// BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY, NON-
+// INFRINGEMENT, OR FITNESS FOR ANY PARTICULAR PURPOSE; and
+// (2) Xilinx shall not be liable (whether in contract or tort,
+// including negligence, or under any other theory of
+// liability) for any loss or damage of any kind or nature
+// related to, arising under or in connection with these
+// materials, including for any direct, or any indirect,
+// special, incidental, or consequential loss or damage
+// (including loss of data, profits, goodwill, or any type of
+// loss or damage suffered as a result of any action brought
+// by a third party) even if such damage or loss was
+// reasonably foreseeable or Xilinx had been advised of the
+// possibility of the same.
+// 
+// CRITICAL APPLICATIONS
+// Xilinx products are not designed or intended to be fail-
+// safe, or for use in any application requiring fail-safe
+// performance, such as life-support or safety devices or
+// systems, Class III medical devices, nuclear facilities,
+// applications related to the deployment of airbags, or any
+// other applications that could lead to death, personal
+// injury, or severe property or environmental damage
+// (individually and collectively, "Critical
+// Applications"). Customer assumes the sole risk and
+// liability of any use of Xilinx products in Critical
+// Applications, subject only to applicable laws and
+// regulations governing limitations on product liability.
+// 
+// THIS COPYRIGHT NOTICE AND DISCLAIMER MUST BE RETAINED AS
+// PART OF THIS FILE AT ALL TIMES.
+// 
+// DO NOT MODIFY THIS FILE.
+
+
+// IP VLNV: xilinx.com:module_ref:zstorm:1.0
+// IP Revision: 1
+
+`timescale 1ns/1ps
+
+(* IP_DEFINITION_SOURCE = "module_ref" *)
+(* DowngradeIPIdentifiedWarnings = "yes" *)
+module design_1_zstorm_0_0 (
+  S_AXI_ACLK,
+  S_AXI_ARESETN,
+  S_AXI_AWADDR,
+  S_AXI_AWPROT,
+  S_AXI_AWVALID,
+  S_AXI_AWREADY,
+  S_AXI_WDATA,
+  S_AXI_WSTRB,
+  S_AXI_WVALID,
+  S_AXI_WREADY,
+  S_AXI_BRESP,
+  S_AXI_BVALID,
+  S_AXI_BREADY,
+  S_AXI_ARADDR,
+  S_AXI_ARPROT,
+  S_AXI_ARVALID,
+  S_AXI_ARREADY,
+  S_AXI_RDATA,
+  S_AXI_RRESP,
+  S_AXI_RVALID,
+  S_AXI_RREADY,
+  m00_axi_aclk,
+  m00_axi_aresetn,
+  m00_axi_awready,
+  m00_axi_awaddr,
+  m00_axi_awlen,
+  m00_axi_awsize,
+  m00_axi_awburst,
+  m00_axi_awlock,
+  m00_axi_awcache,
+  m00_axi_awprot,
+  m00_axi_awvalid,
+  m00_axi_wready,
+  m00_axi_wdata,
+  m00_axi_wstrb,
+  m00_axi_wlast,
+  m00_axi_wvalid,
+  m00_axi_bresp,
+  m00_axi_bvalid,
+  m00_axi_bready,
+  m00_axi_arready,
+  m00_axi_araddr,
+  m00_axi_arlen,
+  m00_axi_arsize,
+  m00_axi_arburst,
+  m00_axi_arlock,
+  m00_axi_arcache,
+  m00_axi_arprot,
+  m00_axi_arvalid,
+  m00_axi_rready,
+  m00_axi_rdata,
+  m00_axi_rresp,
+  m00_axi_rlast,
+  m00_axi_rvalid,
+  data,
+  addr,
+  FC,
+  E,
+  VMA,
+  BG,
+  LDS,
+  UDS,
+  RW,
+  AS,
+  DDIR,
+  ADIR,
+  DOE,
+  AOE,
+  ACTIVITY,
+  RESET,
+  HALT,
+  CLK7M,
+  BERR,
+  VPA,
+  IPL,
+  BGACK,
+  BR,
+  DTACK,
+  clk0,
+  PS_RESET
+);
+
+(* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME S_AXI_ACLK, ASSOCIATED_BUSIF S_AXI, ASSOCIATED_RESET S_AXI_ARESETN, FREQ_HZ 100000000, PHASE 0.000, CLK_DOMAIN design_1_processing_system7_0_0_FCLK_CLK0, INSERT_VIP 0" *)
+(* X_INTERFACE_INFO = "xilinx.com:signal:clock:1.0 S_AXI_ACLK CLK" *)
+input wire S_AXI_ACLK;
+(* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME S_AXI_ARESETN, POLARITY ACTIVE_LOW, INSERT_VIP 0" *)
+(* X_INTERFACE_INFO = "xilinx.com:signal:reset:1.0 S_AXI_ARESETN RST" *)
+input wire S_AXI_ARESETN;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI AWADDR" *)
+input wire [4 : 0] S_AXI_AWADDR;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI AWPROT" *)
+input wire [2 : 0] S_AXI_AWPROT;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI AWVALID" *)
+input wire S_AXI_AWVALID;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI AWREADY" *)
+output wire S_AXI_AWREADY;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI WDATA" *)
+input wire [31 : 0] S_AXI_WDATA;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI WSTRB" *)
+input wire [3 : 0] S_AXI_WSTRB;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI WVALID" *)
+input wire S_AXI_WVALID;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI WREADY" *)
+output wire S_AXI_WREADY;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI BRESP" *)
+output wire [1 : 0] S_AXI_BRESP;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI BVALID" *)
+output wire S_AXI_BVALID;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI BREADY" *)
+input wire S_AXI_BREADY;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI ARADDR" *)
+input wire [4 : 0] S_AXI_ARADDR;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI ARPROT" *)
+input wire [2 : 0] S_AXI_ARPROT;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI ARVALID" *)
+input wire S_AXI_ARVALID;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI ARREADY" *)
+output wire S_AXI_ARREADY;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI RDATA" *)
+output wire [31 : 0] S_AXI_RDATA;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI RRESP" *)
+output wire [1 : 0] S_AXI_RRESP;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI RVALID" *)
+output wire S_AXI_RVALID;
+(* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME S_AXI, DATA_WIDTH 32, PROTOCOL AXI4LITE, FREQ_HZ 100000000, ID_WIDTH 0, ADDR_WIDTH 5, AWUSER_WIDTH 0, ARUSER_WIDTH 0, WUSER_WIDTH 0, RUSER_WIDTH 0, BUSER_WIDTH 0, READ_WRITE_MODE READ_WRITE, HAS_BURST 0, HAS_LOCK 0, HAS_PROT 1, HAS_CACHE 0, HAS_QOS 0, HAS_REGION 0, HAS_WSTRB 1, HAS_BRESP 1, HAS_RRESP 1, SUPPORTS_NARROW_BURST 0, NUM_READ_OUTSTANDING 1, NUM_WRITE_OUTSTANDING 1, MAX_BURST_LENGTH 1, PHASE 0.000, CLK_DOMAIN design_1_processing_system7_0_0_FCLK_CLK0, NUM_READ_THREADS\
+ 4, NUM_WRITE_THREADS 4, RUSER_BITS_PER_BYTE 0, WUSER_BITS_PER_BYTE 0, INSERT_VIP 0" *)
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI RREADY" *)
+input wire S_AXI_RREADY;
+(* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME m00_axi_aclk, ASSOCIATED_BUSIF m00_axi, ASSOCIATED_RESET m00_axi_aresetn, FREQ_HZ 142857132, PHASE 0.000, CLK_DOMAIN design_1_processing_system7_0_0_FCLK_CLK1, INSERT_VIP 0" *)
+(* X_INTERFACE_INFO = "xilinx.com:signal:clock:1.0 m00_axi_aclk CLK" *)
+input wire m00_axi_aclk;
+(* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME m00_axi_aresetn, POLARITY ACTIVE_LOW, INSERT_VIP 0" *)
+(* X_INTERFACE_INFO = "xilinx.com:signal:reset:1.0 m00_axi_aresetn RST" *)
+input wire m00_axi_aresetn;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi AWREADY" *)
+input wire m00_axi_awready;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi AWADDR" *)
+output wire [31 : 0] m00_axi_awaddr;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi AWLEN" *)
+output wire [3 : 0] m00_axi_awlen;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi AWSIZE" *)
+output wire [2 : 0] m00_axi_awsize;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi AWBURST" *)
+output wire [1 : 0] m00_axi_awburst;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi AWLOCK" *)
+output wire m00_axi_awlock;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi AWCACHE" *)
+output wire [3 : 0] m00_axi_awcache;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi AWPROT" *)
+output wire [2 : 0] m00_axi_awprot;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi AWVALID" *)
+output wire m00_axi_awvalid;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi WREADY" *)
+input wire m00_axi_wready;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi WDATA" *)
+output wire [31 : 0] m00_axi_wdata;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi WSTRB" *)
+output wire [3 : 0] m00_axi_wstrb;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi WLAST" *)
+output wire m00_axi_wlast;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi WVALID" *)
+output wire m00_axi_wvalid;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi BRESP" *)
+input wire [1 : 0] m00_axi_bresp;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi BVALID" *)
+input wire m00_axi_bvalid;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi BREADY" *)
+output wire m00_axi_bready;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi ARREADY" *)
+input wire m00_axi_arready;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi ARADDR" *)
+output wire [31 : 0] m00_axi_araddr;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi ARLEN" *)
+output wire [3 : 0] m00_axi_arlen;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi ARSIZE" *)
+output wire [2 : 0] m00_axi_arsize;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi ARBURST" *)
+output wire [1 : 0] m00_axi_arburst;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi ARLOCK" *)
+output wire m00_axi_arlock;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi ARCACHE" *)
+output wire [3 : 0] m00_axi_arcache;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi ARPROT" *)
+output wire [2 : 0] m00_axi_arprot;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi ARVALID" *)
+output wire m00_axi_arvalid;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi RREADY" *)
+output wire m00_axi_rready;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi RDATA" *)
+input wire [31 : 0] m00_axi_rdata;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi RRESP" *)
+input wire [1 : 0] m00_axi_rresp;
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi RLAST" *)
+input wire m00_axi_rlast;
+(* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME m00_axi, DATA_WIDTH 32, PROTOCOL AXI3, FREQ_HZ 142857132, ID_WIDTH 0, ADDR_WIDTH 32, AWUSER_WIDTH 0, ARUSER_WIDTH 0, WUSER_WIDTH 0, RUSER_WIDTH 0, BUSER_WIDTH 0, READ_WRITE_MODE READ_WRITE, HAS_BURST 1, HAS_LOCK 1, HAS_PROT 1, HAS_CACHE 1, HAS_QOS 0, HAS_REGION 0, HAS_WSTRB 1, HAS_BRESP 1, HAS_RRESP 1, SUPPORTS_NARROW_BURST 1, NUM_READ_OUTSTANDING 2, NUM_WRITE_OUTSTANDING 2, MAX_BURST_LENGTH 16, PHASE 0.000, CLK_DOMAIN design_1_processing_system7_0_0_FCLK_CLK1, NUM_READ_THREADS\
+ 1, NUM_WRITE_THREADS 1, RUSER_BITS_PER_BYTE 0, WUSER_BITS_PER_BYTE 0, INSERT_VIP 0" *)
+(* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 m00_axi RVALID" *)
+input wire m00_axi_rvalid;
+inout wire [15 : 0] data;
+output wire [23 : 1] addr;
+output wire [2 : 0] FC;
+output wire E;
+output wire VMA;
+output wire BG;
+output wire LDS;
+output wire UDS;
+output wire RW;
+output wire AS;
+output wire DDIR;
+output wire ADIR;
+output wire DOE;
+output wire AOE;
+output wire ACTIVITY;
+(* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME RESET, POLARITY ACTIVE_LOW, INSERT_VIP 0" *)
+(* X_INTERFACE_INFO = "xilinx.com:signal:reset:1.0 RESET RST" *)
+inout wire RESET;
+inout wire HALT;
+input wire CLK7M;
+input wire BERR;
+input wire VPA;
+input wire [2 : 0] IPL;
+input wire BGACK;
+input wire BR;
+input wire DTACK;
+input wire clk0;
+(* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME PS_RESET, POLARITY ACTIVE_LOW, INSERT_VIP 0" *)
+(* X_INTERFACE_INFO = "xilinx.com:signal:reset:1.0 PS_RESET RST" *)
+input wire PS_RESET;
+
+  zstorm inst (
+    .S_AXI_ACLK(S_AXI_ACLK),
+    .S_AXI_ARESETN(S_AXI_ARESETN),
+    .S_AXI_AWADDR(S_AXI_AWADDR),
+    .S_AXI_AWPROT(S_AXI_AWPROT),
+    .S_AXI_AWVALID(S_AXI_AWVALID),
+    .S_AXI_AWREADY(S_AXI_AWREADY),
+    .S_AXI_WDATA(S_AXI_WDATA),
+    .S_AXI_WSTRB(S_AXI_WSTRB),
+    .S_AXI_WVALID(S_AXI_WVALID),
+    .S_AXI_WREADY(S_AXI_WREADY),
+    .S_AXI_BRESP(S_AXI_BRESP),
+    .S_AXI_BVALID(S_AXI_BVALID),
+    .S_AXI_BREADY(S_AXI_BREADY),
+    .S_AXI_ARADDR(S_AXI_ARADDR),
+    .S_AXI_ARPROT(S_AXI_ARPROT),
+    .S_AXI_ARVALID(S_AXI_ARVALID),
+    .S_AXI_ARREADY(S_AXI_ARREADY),
+    .S_AXI_RDATA(S_AXI_RDATA),
+    .S_AXI_RRESP(S_AXI_RRESP),
+    .S_AXI_RVALID(S_AXI_RVALID),
+    .S_AXI_RREADY(S_AXI_RREADY),
+    .m00_axi_aclk(m00_axi_aclk),
+    .m00_axi_aresetn(m00_axi_aresetn),
+    .m00_axi_awready(m00_axi_awready),
+    .m00_axi_awaddr(m00_axi_awaddr),
+    .m00_axi_awlen(m00_axi_awlen),
+    .m00_axi_awsize(m00_axi_awsize),
+    .m00_axi_awburst(m00_axi_awburst),
+    .m00_axi_awlock(m00_axi_awlock),
+    .m00_axi_awcache(m00_axi_awcache),
+    .m00_axi_awprot(m00_axi_awprot),
+    .m00_axi_awvalid(m00_axi_awvalid),
+    .m00_axi_wready(m00_axi_wready),
+    .m00_axi_wdata(m00_axi_wdata),
+    .m00_axi_wstrb(m00_axi_wstrb),
+    .m00_axi_wlast(m00_axi_wlast),
+    .m00_axi_wvalid(m00_axi_wvalid),
+    .m00_axi_bresp(m00_axi_bresp),
+    .m00_axi_bvalid(m00_axi_bvalid),
+    .m00_axi_bready(m00_axi_bready),
+    .m00_axi_arready(m00_axi_arready),
+    .m00_axi_araddr(m00_axi_araddr),
+    .m00_axi_arlen(m00_axi_arlen),
+    .m00_axi_arsize(m00_axi_arsize),
+    .m00_axi_arburst(m00_axi_arburst),
+    .m00_axi_arlock(m00_axi_arlock),
+    .m00_axi_arcache(m00_axi_arcache),
+    .m00_axi_arprot(m00_axi_arprot),
+    .m00_axi_arvalid(m00_axi_arvalid),
+    .m00_axi_rready(m00_axi_rready),
+    .m00_axi_rdata(m00_axi_rdata),
+    .m00_axi_rresp(m00_axi_rresp),
+    .m00_axi_rlast(m00_axi_rlast),
+    .m00_axi_rvalid(m00_axi_rvalid),
+    .data(data),
+    .addr(addr),
+    .FC(FC),
+    .E(E),
+    .VMA(VMA),
+    .BG(BG),
+    .LDS(LDS),
+    .UDS(UDS),
+    .RW(RW),
+    .AS(AS),
+    .DDIR(DDIR),
+    .ADIR(ADIR),
+    .DOE(DOE),
+    .AOE(AOE),
+    .ACTIVITY(ACTIVITY),
+    .RESET(RESET),
+    .HALT(HALT),
+    .CLK7M(CLK7M),
+    .BERR(BERR),
+    .VPA(VPA),
+    .IPL(IPL),
+    .BGACK(BGACK),
+    .BR(BR),
+    .DTACK(DTACK),
+    .clk0(clk0),
+    .PS_RESET(PS_RESET)
+  );
+endmodule
